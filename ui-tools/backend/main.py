e649e40fb41dda75cdf130ccc247dc8c17e3fec1
jlhofmann-microsoft <jeremyho@microsoft.com>
2026-05-05 14:36:21 -0500

    External Interaction icons.

--- ui-tools/backend/main.py
+++ ui-tools/backend/main.py
@@ -331,5 +331,5 @@ async def get_environments():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="warning", access_log=False)
     
